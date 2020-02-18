0a4306f8fd9d25f2170f3f0515e10661996a9522
toure <maka.toure@crea-inseec.com>
2020-02-18 11:49:51 +0100

    just for my boy Badara

--- resources/views/artists/index.blade.php
+++ resources/views/artists/index.blade.php
@@ -12,7 +12,13 @@
                 <td> {{ $artist->name }} </td>
                 <td> {{ $artist->firstname }} </td>
                 <td class="table-action">  <a type="button" href="{{ route('artist.edit', $artist->id )}}" class="btn btn-sm" data-toggle="tooltip"
-            title="@lang('Edit artist') {{$artist->name }}"> Edit</a></td>
+            title="@lang('Edit artist') {{$artist->name }}"> Edit</a>
+                    
+            <a type="button" href="{{ route('artist.destroy', $artist->id )}}" class="btn btn-danger btn-sm" data-toggle="tooltip"
+                    title="@lang('Delete artist') {{$artist->name }}"> Delete 
+                    <i class="fas fa-trash fa-lg" class="btn-delete"></i>
+            </a>
+        </td>
     
             </tr>
            
@@ -21,3 +27,20 @@
     </table>
     
     {{ $artists->appends(request()->except('page'))->links() }}
+  
+    <script> 
+        $.ajaxSetuo({
+            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
+        });
+
+        $(document).on('click', 'btn-delete' function(){
+            $.ajax({
+                url: $(this).attr('href'),
+                method: 'DELETE'
+            }).done(function(){
+                $(this).closest('tr').remove();
+            })
+            return false; 
+        }); 
+
+    </script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,15 @@
         <title>@yield('title')</title>
 
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!--boostrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+        <!--script jquery -->
+        <script
+        src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+        crossorigin="anonymous">
+        </script>
         <!-- Styles -->
         <style>
             html, body {
